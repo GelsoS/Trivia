2eb9cbe22aee846cc4db2cec9b709b0c5bcf04a7
Davi Albino <negroalbino_@hotmail.com>
2022-08-11 12:36:43 -0300

    requisito 15

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
 class Feedback extends Component {
-  // componentDidMount = () => {
-  //   const { gravatarAPI } = this.props;
-  //   gravatarAPI();
-  // }
+  playAgainBtn = () => {
+    const { history } = this.props;
+    history.push('/');
+  }
 
   render() {
     return (
       <>
         <Header />
         <div data-testid="feedback-text"><h1>Feedback</h1></div>
+        <button
+          type="button"
+          data-testid="btn-play-again"
+          onClick={ this.playAgainBtn }
+        >
+          Play Again
+
+        </button>
       </>
     );
   }
 }
 
-// const mapStateToProps = (state) => ({
-//   name: state.loginReducer.name,
-//   email: state.loginReducer.email,
-// });
+Feedback.propTypes = {
+  history: PropTypes.object,
+}.isRequired;
 
 export default Feedback;
